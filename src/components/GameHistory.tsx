b48ac35ede43b947746091f6fcc2d0ed3535e984
albertoZurini <alberto.zurini@gmail.com>
2024-07-14 06:31:24 +0200

    .

--- src/components/GameHistory.tsx
+++ src/components/GameHistory.tsx
@@ -3,18 +3,24 @@ import './GameHistory.css';
 
 
 const GameHistory: React.FC = () => {
-    const [history, setHistory] = useState([])
+    const [history, setHistory] = useState<string[]>([])
     useEffect(() => {
         getApi()
     },
         [])
     const getApi = () => {
         fetch("https://arbitrum-sepolia.blockscout.com/api/v2/addresses/0x39a2D23C1D2DcCc391942cf88F6cDD9Db9930D74/transactions?filter=to%20%7C%20from")
-            .then(data => data.body)
-            .then(data => {
-                const reader = data?.getReader()
-                return reader?.read()
-            })
+        .then(data => data.json())
+        .then(data => {
+            let parsed : string[] = []
+            for(let item of data.items.reverse()){
+                if(item.method == null) continue;
+                let date = new Date(item.timestamp);
+                parsed.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} - ${item.method}`)
+            }
+            console.log(parsed)
+            setHistory(parsed)
+        })
 
     }
 
